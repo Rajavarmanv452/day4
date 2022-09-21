7a3f1d99433e9a85b3ecb3c850dbea2f6fca5714
RAJAVARMAN V <113004067+Rajavarmanv452@users.noreply.github.com>
2022-09-21 13:13:05 +0530

    que 1,2,3

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//que2 & 3
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+    var res=JSON.parse(request.response);
+    console.log(res);
+    for (var i=0;i<res.length;i++){
+        console.log("country :"+ res[i].name,"capital :"+ res[i].capital,"flags :"+ res[i].flags.png)
+    }
+};
+//que 1
 var obj1 ={name:"person1" ,age:5}
 var obj2 ={age:5 ,name:"person1"}
 if (obj1==obj2) {
